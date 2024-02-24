ca6a0a2d37a2527a6685c1d31f21ddb2e0e74e8d
violeta456 <159485730+violeta456@users.noreply.github.com>
2024-02-23 22:32:40 -0500

    Terminado

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,107 @@
+package taller3.televisores;
+
+public class TV {
+	
+	private Marca marca;
+	private int canal;
+	private int precio;
+	private boolean estado;
+	private int volumen;
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca=marca;
+		this.estado=estado;
+		this.canal=1;
+		this.volumen=1;
+		this.precio=500;
+	
+	}
+	
+	//get y set atributos
+	public Marca getMarca() {
+		return marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	public void setCanal(int canal) {
+		this.canal=canal;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	public void setControl(Control control){
+		this.control=control;
+	}
+	
+	
+	//Métodos de clase
+	public static int getNumTV() {
+		return numTV;
+		
+	}
+	public static int incrementoNumTV() {
+		return numTV++;
+	}
+	
+	//TurnON y TurnOff
+	
+	public void turnOn(){
+		estado= true;
+	}
+	public void turnOff() {
+		estado=false;
+	}
+	public boolean getEstado() {
+		return estado;	
+	}
+	
+	//canalUP - canalDown y volumenUp - volumenDown
+	
+	public void canalUp() {
+		if(canal<120 && estado==true) {
+			canal++;		
+		}
+		
+	}
+	public void canalDown() {
+		if (canal>0 && estado==true) {
+			canal--;
+		}
+		
+	}
+	public void volumenUp() {
+		if(volumen<7 && estado==true) {
+			volumen++;
+		}
+		
+	}
+	public void volumenDown() {
+		if(volumen>0 && estado==true) {
+			volumen--;
+		}
+		
+	}
+
+}
